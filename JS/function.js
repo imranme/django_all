54072ad56037c0319e8240a8e261b3d5284c4fc6
imranme <tusharimran914076@gmail.com>
2024-07-15 17:08:52 +0600

    js function add

---
+++ JS/function.js
@@ -0,0 +1,38 @@
+// function sumTwoNumber(){
+//     console.log("hello i am new in function");
+
+// }
+
+// sumTwoNumber();
+
+// function sumTwoNumber(num1, num2){
+//     console.log(num1+ num2);
+// }
+
+// sumTwoNumber(10, 30)
+
+// function sumTwoNumber(num1  = 0, num2 = 0  ){
+//   var result = num1 + num2;
+//   return result;
+// }
+
+// var output = sumTwoNumber(200, 100);
+
+// function multiply(num){
+//     return output*num
+// }
+
+// var result2 = multiply(2);
+
+// console.log(result2);
+
+function test1(){
+    return"hello"
+}
+
+function test2(){
+    return test1() + " world";
+}
+
+var output2 = test2();
+console.log(output2);
