da244bdcbc8ca6174dbe77cd7f4fb62dee986579
imranme <tusharimran914076@gmail.com>
2024-07-28 16:22:44 +0600

    js function add

---
+++ JS/dom-javascript/app.js
@@ -0,0 +1,24 @@
+// console.log("hello")
+// g keybord sort
+
+var child = document.getElementsByClassName("child");
+
+// console.log(child)
+
+var img = document.getElementById("img");
+
+// console.log(img.getAttribute("src")) get attribute
+
+function createEL(){
+    var p = document.createElement("p");
+    p.innerText = "Notun ami ";
+    newDIv.appendChild(p);
+}
+
+document.getElementById("submit-btn").addEventListener("click", function(e){
+   console.log("yess bosssss");
+
+//    createEL();
+var inputvalue=document.getElementById("input").value;
+console.log(inputvalue);
+});
