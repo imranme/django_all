70bc7f3782a4be808fe689e38660209f6fbf848d
imranme <tusharimran914076@gmail.com>
2024-08-27 07:05:26 +0600

    my project urls part done

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 
+def home(requst):
+    return HttpResponse("This is Home Page")
 def contact(requst):
-    return HttpResponse("This is contact page")
+    return HttpResponse("This is contact page") 
